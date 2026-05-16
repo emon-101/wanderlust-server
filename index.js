6b260ce68aae821d2053b5b1b2025e6b1695bd1e
emon-101 <emon26.ugv@gmail.com>
2026-05-16 22:47:31 +0600

    Add authorization check for retrieving a destination by ID

--- index.js
+++ index.js
@@ -36,7 +36,14 @@ async function run() {
     //   res.json(result);
     // })
 
-    app.get('/destination/:id', async(req, res) => {
+    app.get('/destination/:id', (req, res, next) => {
+      const header = req.headers.authorization
+      if(header === 'logged in') {
+        next();
+      } else {
+        res.status(401).json({message: "Unauthorized"});
+      }
+    }, async(req, res) => {
         const {id} = req.params;
         const result = await destinationCollection.findOne({_id: new ObjectId(id)});
         res.json(result);
